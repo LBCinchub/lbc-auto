89c5f841a1e4cdeb165f93a280400d2cd7d48aea
Mokhtar tarek Samara <mokhtartareksamara@gmail.com>
2026-03-11 01:05:28 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -4,9 +4,14 @@ import Sidebar from "./components/layout/Sidebar";
 export default function Layout({ children, currentPageName }) {
   return (
     <div className="min-h-screen bg-gray-950">
+      <style>{`
+        :root {
+          --sidebar-width: 256px;
+        }
+      `}</style>
       <Sidebar currentPage={currentPageName} />
-      <main className="lg:pl-64 min-h-screen">
-        <div className="p-4 md:p-6 lg:p-8 pt-16 lg:pt-8">
+      <main className="lg:pl-64 min-h-screen transition-all duration-300">
+        <div className="p-4 md:p-6 lg:p-8 pt-16 lg:pt-8 max-w-[1400px]">
           {children}
         </div>
       </main>
